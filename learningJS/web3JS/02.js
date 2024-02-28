62eb6cb88803301d4123fafba007b05863f2cbc1
NongChen1223 <2584533077@qq.com>
2024-02-28 19:52:20 +0800

    更改

--- learningJS/web3JS/02.js
+++ learningJS/web3JS/02.js
@@ -3,8 +3,10 @@ async function init(){
     try {
         const web3 =  new Web3(ethRPC)
 
-    }catch (err){
-        console.log('Web3.JS 报错！！',err)
+    } catch (err) {
+        
+
+        console.log('Web3.JS 报错！！11',err)
     }
 
 }
